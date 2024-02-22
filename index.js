2c744da163c7dc31c4ac0497c49872f449c32e98
Jonas Moura <Jonas_jm@live.com>
2024-02-21 21:18:26 -0300

    Update
    Basic calculator functions added

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-let display = document.getElementById("display");
+const display = document.getElementById("display");
+let result = "";
+
+function isOperator(value) {
+  return ["+", "-", "*", "/"].includes(value);
+}
 
 function attachToDisplay(input) {
+  if (
+    (display.value == "0" ||
+      display.value == result ||
+      display.value == "Error") &&
+    !isOperator(input)
+  ) {
+    display.value = "";
+  }
   display.value += input;
 }
 
-function clear() {
-  display.value = " ";
+function clearDisplay() {
+  display.value = "0";
+}
+
+function equal() {
+  try {
+    display.value = eval(display.value);
+    result = display.value;
+  } catch {
+    display.value = "Error";
+  }
 }
